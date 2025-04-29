3647687d61a0f6fc2c931d8fa25a72913cd2859f
Indranil Mukherjee <rpmukherjee@gmail.com>
2025-04-29 17:05:30 +0530

    added comment For readebility

--- Backend/Src/Utils/AsyncHandler.js
+++ Backend/Src/Utils/AsyncHandler.js
@@ -1,4 +1,4 @@
-
+// Created a Async Handler
 export function AsyncHandler(Controller_Function) {
   return (req, res, next) => {
     Promise.resolve(Controller_Function(req, res, next)).catch((error) => {

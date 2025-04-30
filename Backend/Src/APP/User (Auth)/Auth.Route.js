f056a778ac07f05ac8e00be79496620683216744
Indranil Mukherjee <rpmukherjee@gmail.com>
2025-04-30 22:42:39 +0530

    configured  Register , Email verification and Logout  routes

--- Backend/Src/APP/User (Auth)/Auth.Route.js
+++ Backend/Src/APP/User (Auth)/Auth.Route.js
@@ -1,7 +1,16 @@
 // importing required modules
 import express from "express"
+import { RegisterUser, VerifyEmail, LogOut } from "./Auth.Controller.js"
+import { validate } from "../../Middlewares/Auth.Middleware.js"
+import { registerValidation } from "../../Utils/Auth.Validation.js"
+import { IfLoggedIn } from "../../Middlewares/Auth.Middleware.js"
 
+// Configuring the Router
 const AuthRoutes = express.Router()
 
+// Setting Up the Routes
+AuthRoutes.post('/register', registerValidation(), validate, RegisterUser)
+AuthRoutes.get(`/verify-email/:EmailVerificationToken`,  VerifyEmail)
+AuthRoutes.post(`/Logout`,  IfLoggedIn ,LogOut)
 
 export default AuthRoutes

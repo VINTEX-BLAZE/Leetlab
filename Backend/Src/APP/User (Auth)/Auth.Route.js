c8562c19942f389c9a9a7cf93191625435f2616b
Indranil Mukherjee <rpmukherjee@gmail.com>
2025-05-04 19:11:06 +0530

    Configured  Logout, get-Profile, Logout, Forgot-Password, Reset-Password, Reset-Current-Password Routes

--- Backend/Src/APP/User (Auth)/Auth.Route.js
+++ Backend/Src/APP/User (Auth)/Auth.Route.js
@@ -1,6 +1,6 @@
 // importing required modules
 import express from "express"
-import { RegisterUser, VerifyEmail, LogOut } from "./Auth.Controller.js"
+import { RegisterUser, VerifyEmail, LogOut, GetProfile, ForgotPasswordRequest, ResetForgotenPassword, ChangeCurrentPassword } from "./Auth.Controller.js"
 import { validate } from "../../Middlewares/Auth.Middleware.js"
 import { registerValidation } from "../../Utils/Auth.Validation.js"
 import { IfLoggedIn } from "../../Middlewares/Auth.Middleware.js"
@@ -11,6 +11,11 @@ const AuthRoutes = express.Router()
 // Setting Up the Routes
 AuthRoutes.post('/register', registerValidation(), validate, RegisterUser)
 AuthRoutes.get(`/verify-email/:EmailVerificationToken`,  VerifyEmail)
-AuthRoutes.post(`/Logout`,  IfLoggedIn ,LogOut)
+AuthRoutes.post(`/Logout`, IfLoggedIn, LogOut)
+AuthRoutes.post('/get-Profile', IfLoggedIn, GetProfile)
+AuthRoutes.post('/Logout', IfLoggedIn, LogOut)
+AuthRoutes.post('/Forgot-Password', ForgotPasswordRequest)
+AuthRoutes.post('/Reset-Password/:Token', ResetForgotenPassword)
+AuthRoutes.post('/Reset-Current-Password', IfLoggedIn, ChangeCurrentPassword)
 
 export default AuthRoutes
